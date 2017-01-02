1963c0cc8915d64e49b3db402c262ab2a73f7a2f
Claudio Zizza <zizza@flagbit.de>
2017-01-02 18:31:57 +0100

    [PIMKOEMPF-40] Configure validation rule in TableType

--- src/AttributeType/TableType.php
+++ src/AttributeType/TableType.php
@@ -8,12 +8,18 @@ use Pim\Component\Catalog\Model\AttributeInterface;
 class TableType extends AbstractAttributeType
 {
     const FLAGBIT_CATALOG_TABLE = 'flagbit_catalog_table';
+
     /**
      * {@inheritdoc}
      */
     protected function defineCustomAttributeProperties(AttributeInterface $attribute)
     {
         $properties = parent::defineCustomAttributeProperties($attribute) + [
+            'validationRule' => [
+                'name'      => 'validationRule',
+                'fieldType' => 'hidden',
+                'data'   => 'flagbit_table_rule',
+            ],
             'minimumInputLength' => [
                 'name'      => 'minimumInputLength',
                 'fieldType' => 'pim_number'
