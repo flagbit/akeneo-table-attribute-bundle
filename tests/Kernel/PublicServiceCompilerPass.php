4b1640dc76845741f4f25b31dbd690c6dafd6380
leavoelk <lea.voelk@flagbit.de>
2019-05-08 15:29:07 +0200

    [phpunit] Add PublicServiceCompilerPass for integration test
    To test private services in symfony 3.4, all affected services have to be
    set to public.

---
+++ tests/Kernel/PublicServiceCompilerPass.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Flagbit\Bundle\TableAttributeBundle\Test\Kernel;
+
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class PublicServiceCompilerPass implements CompilerPassInterface
+{
+
+    /**
+     * @var array
+     */
+    private $serviceIds;
+
+    public function __construct(array $serviceIds)
+    {
+
+        $this->serviceIds = $serviceIds;
+    }
+
+    public function process(ContainerBuilder $container)
+    {
+        foreach ($this->serviceIds as $serviceId) {
+            $container->getDefinition($serviceId)->setPublic(true);
+        }
+    }
+}
