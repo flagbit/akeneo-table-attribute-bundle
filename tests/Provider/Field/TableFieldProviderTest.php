668fef8a0daa5365f8ba17e57e26015d9f2e55df
leavoelk <lea.voelk@flagbit.de>
2019-05-10 12:21:57 +0200

    [phpunit] Add integrationtest for provider.field

--- tests/Provider/Field/TableFieldProviderTest.php
+++ tests/Provider/Field/TableFieldProviderTest.php
@@ -2,10 +2,23 @@
 
 namespace Flagbit\Bundle\TableAttributeBundle\Test\Provider\Field;
 
-use Flagbit\Bundle\TableAttributeBundle\Provider\Field\TableFieldProvider;
+use Pim\Bundle\CatalogBundle\Entity\Attribute;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class TableFieldProviderTest extends KernelTestCase
 {
+    public function testFieldProviderIsCompatible()
+    {
+        self::bootKernel();
+        $container = self::$kernel->getContainer();
 
+        $chainedFieldProvider = $container->get('pim_enrich.provider.field.chained');
+
+        $attribute = new Attribute();
+        $attribute->setAttributeType('flagbit_catalog_table');
+
+        $fieldProvider = $chainedFieldProvider->getField($attribute);
+
+        self::assertSame('flagbit-table-field', $fieldProvider);
+    }
 }
