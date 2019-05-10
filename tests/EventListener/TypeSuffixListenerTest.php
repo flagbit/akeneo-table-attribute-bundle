656c01383a63a444cdb916f5331572cbca3d810c
leavoelk <lea.voelk@flagbit.de>
2019-05-10 10:08:59 +0200

    [phpunit] Add testfiles and folder structure for integrationtests

---
+++ tests/EventListener/TypeSuffixListenerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Flagbit\Bundle\TableAttributeBundle\Test\EventListener;
+
+use Flagbit\Bundle\TableAttributeBundle\EventListener\TypeSuffixListener;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class TypeSuffixListenerTest extends KernelTestCase
+{
+
+}
---
+++ tests/Provider/Field/TableFieldProviderTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Flagbit\Bundle\TableAttributeBundle\Test\Provider\Field;
+
+use Flagbit\Bundle\TableAttributeBundle\Provider\Field\TableFieldProvider;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class TableFieldProviderTest extends KernelTestCase
+{
+
+}
