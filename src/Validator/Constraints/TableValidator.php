d57bb976c23eebff2ce5ca0a7af9d99ea3af5272
Angel Vazquez <angel.vazquez@flagbit.de>
2019-05-28 14:23:43 +0200

    Fix wrong return type on comment #12

--- src/Validator/Constraints/TableValidator.php
+++ src/Validator/Constraints/TableValidator.php
@@ -9,7 +9,7 @@ use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 class TableValidator extends AllValidator
 {
     /**
-     * @param mixed      $value
+     * @param string     $value
      * @param Constraint $constraint
      */
     public function validate($value, Constraint $constraint)
