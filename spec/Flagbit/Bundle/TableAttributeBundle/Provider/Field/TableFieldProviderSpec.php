535b209adb4043ecb2f48b7199333dacd2dcf90c
Amit Singh <amit.singh@flagbit.de>
2019-06-26 10:08:21 +0200

    Update specs for TableFieldProvider #12

--- spec/Flagbit/Bundle/TableAttributeBundle/Provider/Field/TableFieldProviderSpec.php
+++ spec/Flagbit/Bundle/TableAttributeBundle/Provider/Field/TableFieldProviderSpec.php
@@ -4,6 +4,8 @@ namespace spec\Flagbit\Bundle\TableAttributeBundle\Provider\Field;
 
 use Flagbit\Bundle\TableAttributeBundle\Provider\Field\TableFieldProvider;
 use PhpSpec\ObjectBehavior;
+use Akeneo\Pim\Structure\Component\Model\AttributeInterface;
+
 
 class TableFieldProviderSpec extends ObjectBehavior
 {
@@ -18,5 +20,21 @@ class TableFieldProviderSpec extends ObjectBehavior
             'foo' => 'bar',
         ];
         $this->getField($element)->shouldReturn('flagbit-table-field');
+    }
+
+    public function it_checks_correct_support
+    (
+        AttributeInterface $attributeInterface
+    ){
+        $attributeInterface->getType()->willReturn('flagbit_catalog_table');
+        $this->supports($attributeInterface)->shouldReturn(true);
+    }
+
+    public function it_checks_incorrect_support
+    (
+        AttributeInterface $attributeInterface
+    ){
+        $attributeInterface->getType()->willReturn('foo_bar');
+        $this->supports($attributeInterface)->shouldReturn(false);
     }
 }
