28afad3eb4a732a2962f198908f541593cfe0178
Claudio Zizza <zizza@flagbit.de>
2020-10-13 13:59:42 +0200

    Fix of select product export filter
    Because this bundle overrides the Akeneo AttributeOption entity
    it is important for the product export select filter to use this
    class too.

---
+++ jest/integration/formextensions.test.js
@@ -0,0 +1,22 @@
+describe('Form Extensions', function() {
+    it('table attribute overrides akeneo-attribute-select-filter', function() {
+        const formExtensions = require('../../tests/public/js/extensions.json');
+
+        const expected = {
+            module: 'pim/filter/attribute/select',
+            parent: null,
+            targetZone: 'self',
+            zones: [],
+            aclResourceId: null,
+            config: {
+                url: 'pim_ui_ajaxentity_list',
+                entityClass: 'Flagbit\\Bundle\\TableAttributeBundle\\Entity\\AttributeOption',
+                operators: [ 'IN', 'EMPTY', 'NOT EMPTY' ]
+            },
+            position: 100,
+            code: 'akeneo-attribute-select-filter'
+        };
+
+        expect(formExtensions.extensions).toContainEqual(expected);
+    });
+});
